2b60e3a3a7f1f136eaaf3a67be8ebc22af9eca2b
vanessaestanislau <estanislaunessau@gmail.com>
2022-06-09 23:27:06 -0300

    first commit

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,44 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double a, b;
+
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+		double divisao = divisao(a, b);
+
+		System.out.println("O resultado da soma entre os valores digitados é: " + soma);
+		System.out.println("O resultado da subtração entre os valores digitados é: " + subtracao);
+		System.out.println("O resultado da multiplicação entre os valores digitados é: " + multiplicacao);
+		System.out.println("O resultado da divisão entre os valores digitados é: " + divisao);
+
+	}
+
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+
+	public static double subtracao(double a, double b) {
+		return a - b;
+	}
+
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+	public static double divisao(double a, double b) {
+		return a / b;
+	}
+}
